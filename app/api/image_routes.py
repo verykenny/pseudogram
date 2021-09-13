57025c063731486400fcb50566e935ca5c0cb4a2
verykenny <16845292+verykenny@users.noreply.github.com>
2021-09-13 10:56:22 -0600

    returned json string and confirmed route works

--- app/api/image_routes.py
+++ app/api/image_routes.py
@@ -8,5 +8,6 @@ image_routes = Blueprint('images', __name__)
 @image_routes.route('/')
 def image_feed():
     images = Image.query.all()
+    images[0].createdAt = images[0].createdAt.strftime('%m/%d/%Y')
 
-    return {'images': [image.to_dict() for image in images]}
+    return {'images': images[0].to_dict()}
--- react-app/src/components/ImageFeed/ImageFeed.js
+++ react-app/src/components/ImageFeed/ImageFeed.js
@@ -1,7 +1,18 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux"
+import { get_feed } from "../../store/feed";
 
 
 const ImageFeed = () => {
+    const images = useSelector(state => state.feed)
+    const dispatch = useDispatch()
+
+
+    useEffect(() => {
+        dispatch(get_feed());
+    }, [dispatch])
+
+
     return (
         <>
             <h1>Image Feed</h1>
