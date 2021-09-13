5bc6d6d9b6e1cf74c5965eeb910d5fb63613bc70
verykenny <16845292+verykenny@users.noreply.github.com>
2021-09-13 11:09:18 -0600

    added actions for loading and failure to load

--- react-app/src/store/feed.js
+++ react-app/src/store/feed.js
@@ -1,29 +1,68 @@
-const SET_FEED = 'feed/SET_FEED'
+const SET_FEED_BEGIN = 'feed/SET_FEED_BEGIN'
+const SET_FEED_FAIL = 'feed/SET_FEED_FAIL'
+const SET_FEED_SUCCESS = 'feed/SET_FEED_SUCCESS'
 
-const setFeed = (feed) => ({
-    type: SET_FEED,
-    payload: feed
+
+const setFeedBegin = () => ({
+    type: SET_FEED_BEGIN
+});
+
+const setFeedFail = (error) => ({
+    type: SET_FEED_FAIL,
+    payload: error
+});
+
+const setFeedSuccess = (feed) => ({
+    type: SET_FEED_SUCCESS,
+    payload: feed.images
 });
 
 
-const initialState = {}
+
+
+const initialState = {
+    images: {},
+    loading: false,
+    errors: null
+}
 
 export const get_feed = () => async (dispatch) => {
+    dispatch(setFeedBegin());
     const response = await fetch('/api/image_feed')
 
     if (response.ok) {
         const data = await response.json();
         if (data.errors) {
+            dispatch(setFeedFail('error'))
             return;
         }
-        dispatch(setFeed(data))
+        dispatch(setFeedSuccess(data))
     }
+
 }
 
 export default function reducer(state = initialState, action) {
     switch (action.type) {
-        case SET_FEED:
-            return { feed: action.payload }
+        case SET_FEED_BEGIN:
+            return {
+                ...state,
+                loading: true,
+                errors: null
+            }
+        case SET_FEED_FAIL:
+            return {
+                ...state,
+                images: {},
+                loading: false,
+                errors: action.payload
+            }
+        case SET_FEED_SUCCESS:
+            return {
+                ...state,
+                loading: false,
+                errors: null,
+                images: action.payload
+            }
         default:
             return state;
     }
