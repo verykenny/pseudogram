e29fcdeef189f8f1112739e1a607aab5f9b10405
verykenny <16845292+verykenny@users.noreply.github.com>
2021-09-13 11:39:49 -0600

    updated ImageFeed component to render images

--- react-app/src/components/ImageFeed/ImageFeed.js
+++ react-app/src/components/ImageFeed/ImageFeed.js
@@ -17,12 +17,13 @@ const ImageFeed = () => {
         <>
             <h1>Image Feed</h1>
             {feed.loading === true && <h2>Loading</h2>}
-            {/* {feed?.images && feed.images?.map(image => (
+            {feed.images && Object.values(feed.images).map(image => (
                 <>
                     <p>{image.caption}</p>
                     <p>{image.userId}</p>
+                    <img src={image.imgUrl} alt={image.caption}></img>
                 </>
-            ))} */}
+            ))}
         </>
     )
 }
