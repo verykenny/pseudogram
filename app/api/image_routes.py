e1e3bb69a634278dc44fad6a7e9fab63652a6709
verykenny <16845292+verykenny@users.noreply.github.com>
2021-09-13 13:52:53 -0600

    updated api route handler for image feed

--- app/api/image_routes.py
+++ app/api/image_routes.py
@@ -1,20 +1,23 @@
 from flask import Blueprint, request
-from app.models import Image
+from app.models import Image, User
 from flask_login import current_user, login_required
 
 
 image_routes = Blueprint('images', __name__)
 
 
-@image_routes.route('/')
-def image_feed():
+@image_routes.route('/images')
+@login_required
+def another_image_feed():
     """
     Return list of images created by session user and their followers
     """
     sessionUserId = current_user.get_id()
-    print('**********************************', sessionUserId)
-    print('**********************************', request.headers)
+    user = User.query.get(sessionUserId)
+    followings = [user.id for user in user.following]
 
-    images = Image.query.filter_by(userId=sessionUserId).all()
+    images = Image.query.filter(Image.userId == sessionUserId).all()
+    followingsImages = Image.query.filter(Image.userId.in_(followings)).all()
+    images.extend(followingsImages)
 
     return {'images': [image.to_dict() for image in images]}
--- app/api/user_routes.py
+++ app/api/user_routes.py
@@ -8,7 +8,6 @@ user_routes = Blueprint('users', __name__)
 @user_routes.route('/')
 @login_required
 def users():
-    print('**********************************', current_user.is_authenticated)
     users = User.query.all()
     return {'users': [user.to_dict() for user in users]}
 
