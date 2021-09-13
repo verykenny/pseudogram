dbafee80c7d73e5fa6847f4aad20b25dccbe3726
verykenny <16845292+verykenny@users.noreply.github.com>
2021-09-13 17:17:39 -0600

    added delete button to image edit form

--- react-app/src/components/ImageEditForm/index.js
+++ react-app/src/components/ImageEditForm/index.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
-import { useParams } from 'react-router-dom'
+import { useParams, useHistory } from 'react-router-dom'
 import { useDispatch, useSelector } from "react-redux"
-import { update_image } from "../../store/feed";
+import { update_image, delete_image } from "../../store/feed";
 
 
 
@@ -9,33 +9,41 @@ import { update_image } from "../../store/feed";
 const ImageEditForm = () => {
     const { imageId } = useParams()
     const image = useSelector(state => state.feed.images[imageId])
-    console.log(image);
-    const [caption, setCaption] = useState(image.caption)
+    const [caption, setCaption] = useState(image?.caption)
     const dispatch = useDispatch()
+    const history = useHistory()
 
     const handleImagePost = (e) => {
         e.preventDefault()
         dispatch(update_image(imageId, caption))
     }
 
+    const handleDeleteImage = (imgId) => {
+        dispatch(delete_image(imgId))
+        history.push('/home')
+    }
+
     return (
         <>
             <h1>Image Update Form</h1>
-                <>
-                    <h1>update with info</h1>
-                    <img className='updating' src={image?.imgUrl} alt='to be updated'></img>
-                    <form onSubmit={handleImagePost}>
-                        <div>
-                            <textarea
-                                value={caption}
-                                onChange={(e) => setCaption(e.target.value)}
-                            ></textarea>
-                        </div>
-                        <div>
-                            <button type='submit'>Update</button>
-                        </div>
-                    </form>
-                </>
+            <>
+                <h1>update with info</h1>
+                <img className='updating' src={image?.imgUrl} alt='to be updated'></img>
+                <form onSubmit={handleImagePost}>
+                    <div>
+                        <textarea
+                            value={caption}
+                            onChange={(e) => setCaption(e.target.value)}
+                        ></textarea>
+                    </div>
+                    <div>
+                        <button type='submit'>Update</button>
+                    </div>
+                </form>
+                <div>
+                    <button onClick={() => handleDeleteImage(imageId)} type='submit'>Delete</button>
+                </div>
+            </>
         </>
     )
 }
