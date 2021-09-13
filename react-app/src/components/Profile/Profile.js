a762d66d6542d7d4ac668b97817860d7a0eec99c
verykenny <16845292+verykenny@users.noreply.github.com>
2021-09-13 14:34:08 -0600

    added context for modal components

--- react-app/src/components/Profile/Profile.js
+++ react-app/src/components/Profile/Profile.js
@@ -1,7 +1,5 @@
 import React from "react";
-import { NavLink, Route } from 'react-router-dom';
-
-// import { useDispatch } from "react-redux"
+import { NavLink } from 'react-router-dom';
 
 const Profile = () => {
 
