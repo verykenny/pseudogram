6ac5ff111dea3bb91cddc5902d99cef9f9c8fe5d
verykenny <16845292+verykenny@users.noreply.github.com>
2021-09-14 15:58:29 -0600

    refactor

--- react-app/src/components/Profile/Profile.js
+++ react-app/src/components/Profile/Profile.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { useSelector} from "react-redux";
+import { useSelector } from "react-redux";
 import { useParams } from 'react-router-dom';
 
 import ImageUploadModal from "../ImageUploadModals";
