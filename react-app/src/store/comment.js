e5550200eb83a92868f8fab185024a6275db3050
verykenny <16845292+verykenny@users.noreply.github.com>
2021-09-14 16:21:03 -0600

    added thunk action creator to updated state when creating new comment"

--- react-app/src/store/comment.js
+++ react-app/src/store/comment.js
@@ -19,6 +19,19 @@ const setCommentsSuccess = ({ comments }) => ({
 })
 
 
+const setNewCommentBegin = () => ({
+    type: SET_NEW_COMMENT_BEGIN,
+})
+const setNewCommentFail = (error) => ({
+    type: SET_NEW_COMMENT_FAIL,
+    payload: error
+})
+const setNewCommentSuccess = ({ comment }) => ({
+    type: SET_NEW_COMMENT_SUCCESS,
+    payload: comment
+})
+
+
 
 const initialState = {
     comments: {},
@@ -40,8 +53,33 @@ export const get_comments = (userId) => async (dispatch) => {
     }
 }
 
+export const set_new_comment = (content, imgId) => async (dispatch) => {
+    dispatch(setNewCommentBegin());
+    const response = await fetch(`/api/image_feed/${imgId}/comments/create`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            content
+        }),
+    })
+
+    if (response.ok) {
+        const data = await response.json();
+        if (data.errors) {
+            dispatch(setNewCommentFail('error'))
+            return;
+        }
+        dispatch(setNewCommentSuccess(data))
+    }
+}
+
+
+
 
 export default function reducer(state = initialState, action) {
+    let newState;
     switch (action.type) {
         case SET_COMMENTS_BEGIN:
             return {
@@ -66,6 +104,26 @@ export default function reducer(state = initialState, action) {
                 errors: null,
                 comments: commentObjs
             };
+        case SET_NEW_COMMENT_BEGIN:
+            return {
+                ...state,
+                loading: true,
+                errors: null
+            };
+        case SET_NEW_COMMENT_FAIL:
+            return {
+                ...state,
+                loading: false,
+                errors: action.payload
+            };
+        case SET_NEW_COMMENT_SUCCESS:
+            newState = {
+                ...state,
+                loading: false,
+                errors: null
+            }
+            newState['comments'][action.payload.id] = action.payload
+            return newState;
         default:
             return state;
     }
