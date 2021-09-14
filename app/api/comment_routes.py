b2ceb49d496d00d220473dac444c9e0ab9c29b30
verykenny <16845292+verykenny@users.noreply.github.com>
2021-09-14 12:16:05 -0600

    added api route for all comments by user and their friends

--- app/api/comment_routes.py
+++ app/api/comment_routes.py
@@ -9,4 +9,9 @@ comment_routes = Blueprint('comments', __name__)
 @comment_routes.route('/')
 @login_required
 def get_comments():
-    return {'users': 'success'}
+    user = User.query.get(current_user.get_id())
+    user_comments = [comment.to_dict() for comment in user.comment]
+    following = [user.id for user in user.following]
+    followings_comments = Comment.query.filter(Comment.userId.in_(following)).all()
+
+    return {'comments': [comment.to_dict() for comment in followings_comments] + user_comments}
