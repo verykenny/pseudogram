269766ef24e453758e4e060d1e0e47291449c82d
verykenny <16845292+verykenny@users.noreply.github.com>
2021-09-14 14:51:11 -0600

    added route to delete an image

--- app/api/likes_routes.py
+++ app/api/likes_routes.py
@@ -1,6 +1,6 @@
 from flask import Blueprint
 from flask_login import login_required, current_user
-from app.models import Like, User
+from app.models import db, Like, User
 
 likes_routes = Blueprint('likes', __name__)
 
@@ -16,7 +16,7 @@ def validation_errors_to_error_messages(validation_errors):
     return errorMessages
 
 
-@likes_routes.route('/')
+@likes_routes.route('')
 @login_required
 def get_likes():
     user = User.query.get(current_user.get_id())
@@ -25,3 +25,11 @@ def get_likes():
     other_likes = Like.query.filter(Like.userId.in_(followings)).all()
     likes = likes + other_likes
     return {'likes': [like.to_dict() for like in likes]}
+
+
+@likes_routes.route('/<int:likeId>', methods=['DELETE'])
+def un_like(likeId):
+    like = Like.query.get(likeId)
+    db.session.delete(like)
+    db.session.commit()
+    return {'like': like.to_dict()}
