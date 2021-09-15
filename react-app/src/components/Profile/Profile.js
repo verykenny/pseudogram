46af441950538b04830e34190863dcb6997d198b
verykenny <16845292+verykenny@users.noreply.github.com>
2021-09-14 19:42:14 -0600

    added view of images on profile page

--- react-app/src/components/Profile/Profile.js
+++ react-app/src/components/Profile/Profile.js
@@ -1,13 +1,23 @@
-import React from "react";
-import { useSelector } from "react-redux";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { useParams } from 'react-router-dom';
+import { get_feed } from "../../store/feed";
 
 import ImageUploadModal from "../ImageUploadModals";
 
+import './Profile.css'
+
 const Profile = () => {
     const user = useSelector(state => state.session.user);
+    const images = useSelector(state => Object.values(state.feed.images))
     const { userId } = useParams();
+    const dispatch = useDispatch()
 
+    useEffect(() => {
+        (async () => {
+            await dispatch(get_feed());
+        })();
+    }, [dispatch]);
 
     return (
         <>
@@ -26,6 +36,13 @@ const Profile = () => {
                 <li>
                     <strong>Email</strong> {user.email}
                 </li>
+                {images.length > 0 && images.map(image => {
+                    if (image.userId === user.id) {
+                        return (
+                            <img className='profile-page-img' src={image.imgUrl} alt={image.caption} />
+                        )
+                    }
+                })}
             </ul>
         </>
     )
