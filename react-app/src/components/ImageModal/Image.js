2da6aea3766f12d3db7c2ad2492056b59128d647
verykenny <16845292+verykenny@users.noreply.github.com>
2021-09-15 17:04:14 -0600

    refactored how the modal was getign image information

--- react-app/src/components/ImageModal/Image.js
+++ react-app/src/components/ImageModal/Image.js
@@ -5,11 +5,8 @@ import { get_comments } from "../../store/comment";
 import './Image.css'
 
 
-
-
-const Image = ({ setShowModal, image, user, setImageModalShow }) => {
-    const { comments } = useSelector(state => state)
-    const dispatch = useDispatch()
+const Image = ({ setShowModal, imageId, user, setImageModalShow }) => {
+    const image = useSelector(state => state.feed.images[imageId])
 
     const closeModal = () => {
         if (setShowModal) setShowModal(false);
@@ -17,25 +14,19 @@ const Image = ({ setShowModal, image, user, setImageModalShow }) => {
         return;
     }
 
-
-    useEffect(() => {
-        dispatch(get_comments())
-    }, [dispatch])
-
-    console.log(comments);
     return (
-
         <>
 
             <div className='exit-bar__image_upload'>
                 <i className="far fa-image"></i>
-                <h2 className='modal-header__image_upload'>Post</h2>
+                <h2 className='modal-header__image_upload'>{`${user.username}'s Post`}</h2>
                 <i onClick={() => closeModal()} className="far fa-window-close"></i>
 
             </div>
             <div className='image-post-container__image_modal'>
                 <div className='display-container__image_modal'>
-                    <img className='image__image_modal' src={image.imgUrl} alt='to be uploaded'></img>
+                    <img className='image__image_modal' src={image?.imgUrl} alt='to be uploaded'></img>
+                    <p>{image.caption}</p>
                 </div>
                 <div className='caption-share-container__image_modal'>
                     <div className='share-container-user-info__image_modal'>
@@ -43,17 +34,11 @@ const Image = ({ setShowModal, image, user, setImageModalShow }) => {
                             { backgroundImage: `url(${user.profileImgUrl})` }
                         }></div>
                         <p>{user.username}</p>
-                        <p>{image.caption}</p>
                     </div>
                     <div className='comments_container__image_modal'>
-                        <div className='image-caption-container__image_modal'>
-                            {comments.loading && <div>Loading...</div>}
-                            {!comments.loading && Object.values(comments?.comments).map(comment => {
-                                if (comment.imgId === image.id) {
-                                    return <CommentCard comment={comment} />
-                                } else {
-                                    return ''
-                                }
+                        <div className='comment-section__image_modal'>
+                            {Object.values(image.comments).map(comment => {
+                                return <CommentCard comment={comment} />
                             })}
                         </div>
                         <div className='share-button-container__image_modal'>
@@ -71,7 +56,14 @@ const Image = ({ setShowModal, image, user, setImageModalShow }) => {
 const CommentCard = ({ comment }) => {
     return (
         <>
-
+            <div className='comment-card__image_modal'>
+                <div className='user-profile-thumb__image_modal' style={
+                    { backgroundImage: `url(${comment.commenter.profileImgUrl})` }
+                }></div>
+                    <div>
+                        <p><span className='username__image_modal'>{comment.commenter.username}</span> {comment.content}</p>
+                    </div>
+            </div>
         </>
     )
 
