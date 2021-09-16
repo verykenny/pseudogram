c3043c40e34654f96624884a8c2ef1c8d65fabc8
verykenny <16845292+verykenny@users.noreply.github.com>
2021-09-16 13:27:50 -0600

    removed reference to users list

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -37,9 +37,6 @@ function App() {
                 <Route path='/sign-up' exact={true}>
                     <SignUpForm />
                 </Route>
-                <ProtectedRoute path='/users' exact={true} >
-                    <UsersList />
-                </ProtectedRoute>
                 <ProtectedRoute path='/users/:userId' exact={true} >
                     <Profile />
                 </ProtectedRoute>
