233649575a62552aba3747a18ebe972307a21d96
verykenny <16845292+verykenny@users.noreply.github.com>
2021-09-16 13:03:18 -0600

    removed delete button from edit

--- react-app/src/components/ImageEditModal/ImageEditForm.js
+++ react-app/src/components/ImageEditModal/ImageEditForm.js
@@ -24,39 +24,35 @@ const ImageEditForm = ({ setShowModal, imageId }) => {
         dispatch(delete_image(image.id))
     }
 
-
     return (
-        <div className='image-upload-modal__image-upload'>
-                    <div className='exit-bar__image_upload'>
+        <div className='image-edit-modal__image_edit'>
+                    <div className='exit-bar__image_edit'>
                         <i className="fas fa-arrow-left"></i>
-                        <h2 className='modal-header__image_upload'>Update your caption</h2>
+                        <h2 className='modal-header__image_edit'>Update your caption</h2>
                         <i onClick={() => setShowModal(false)} className="far fa-window-close"></i>
                     </div>
-                    <div className='container__image_upload image-post-container__image_upload'>
-                        <div className='display-container__image_upload'>
-                            <img className='image-to-upload__image_upload' src={image?.imgUrl} alt='to be uploaded'></img>
+                    <div className='container__image_edit image-post-container__image_edit'>
+                        <div className='display-container__image_edit'>
+                            <img className='image-to-upload__image_edit' src={image?.imgUrl} alt='to be edited'></img>
                         </div>
-                        <div className='caption-share-container__image_upload'>
-                            <div className='share-container-user-info__image_upload'>
-                                <div className='user-profile-thumb__image_upload' style={
+                        <div className='caption-share-container__image_edit'>
+                            <div className='share-container-user-info__image_edit'>
+                                <div className='user-profile-thumb__image_edit' style={
                                     { backgroundImage: `url(${user?.profileImgUrl})` }
                                 }></div>
                                 <p>{user?.username}</p>
                             </div>
-                            <form className='caption-share-form__image_upload' onSubmit={handleImagePost}>
-                                <div className='image-caption-container__image_upload'>
+                            <form className='caption-share-form__image_edit' onSubmit={handleImagePost}>
+                                <div className='image-caption-container__image_edit'>
                                     <textarea
                                         value={caption}
                                         onChange={(e) => setCaption(e.target.value)}
-                                        className='caption-input__image_upload'
+                                        className='caption-input__image_edit'
                                         placeholder='Write a caption...'
                                     ></textarea>
                                 </div>
-                                <div className='share-button-container__image_upload'>
-                                    <button className='share-button__image_upload' type='submit'>Update</button>
-                                </div>
-                                <div className='share-button-container__image_upload'>
-                                    <button className='share-button__image_upload' onClick={handleDeleteImage} >Delete</button>
+                                <div className='share-button-container__image_edit'>
+                                    <button className='share-button__image_edit' type='submit'>Update</button>
                                 </div>
                             </form>
                         </div>
