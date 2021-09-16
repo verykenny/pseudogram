40ac66acdc0e37d4f5c4fa08d6a65f0e99946b2f
arath1869 <arathvidrio@gmail.com>
2021-09-16 20:59:16 +0000

    arath-commit

---
+++ react-app/src/components/FeedImageContainer/index.js
@@ -0,0 +1,9 @@
+const ImageFeedContainer = () => {
+    return (
+        <div>
+            <div className="image-container"></div>
+        </div>
+    )
+}
+
+export default ImageFeedContainer
--- react-app/src/components/UsersWhoLikedModal/UsersWhoLikedModal.js
+++ react-app/src/components/UsersWhoLikedModal/UsersWhoLikedModal.js
@@ -1,77 +1,36 @@
 import React, { useEffect, useState } from 'react';
-import { useDispatch, useSelector } from "react-redux"
-import { NavLink, Link } from "react-router-dom";
-import { get_followings } from "../../store/following"
 import './UsersWhoLiked.css'
 
 function UsersWhoLiked(showProp) {
-    const user = useSelector(state => state.session.user)
-    const following = useSelector(state => state.following)
-    const [users, setUsers] = useState([]);
-    const [likesArray, setLikesArray] = useState([]);
+    let newArray = showProp.props.likes
+    console.log('new',newArray)
 
-    const dispatch = useDispatch()
-
-    useEffect(() => {
-        async function fetchData() {
-            const response = await fetch('/api/users/');
-            const responseData = await response.json();
-            setUsers(responseData.users);
-        }
-        fetchData();
-    }, []);
-
-    useEffect(() => {
-        async function fetchData() {
-            const response = await fetch('/api/likes');
-            const responseData = await response.json();
-            setLikesArray(responseData.likes);
-        }
-        fetchData();
-    }, []);
-
-    useEffect(() => {
-        (async () => {
-            await dispatch(get_followings(user.id));
-        })();
-    }, [dispatch]);
-
-    let arrayOfId = []
-    users.forEach(element => {
-        arrayOfId.push(element.id)
-    })
-
-    let imageId = showProp.props
-
-    let arrayOfImageLikes = []
-    likesArray.forEach(element => {
-        if (element.imgId.id === imageId) {
-            arrayOfImageLikes.push(element.user)
-        }
+    newArray.forEach(element => {
+        console.log(element.user.username)
     })
 
 
     return (
         <div className="user-who-liked__liked_modal">
             <div className="users-who-liked-container">
-                {arrayOfImageLikes.length === 0 &&
+                {newArray.length === 0 &&
                     <div>
                         <div className="top-menu">Likes</div>
                         <div className="users-who-liked-loading">
                         </div>
                     </div>
                 }
-                {arrayOfImageLikes.length !== 0 &&
+                {newArray.length !== 0 &&
                     <div>
                         <div className="top-menu">Likes</div>
                         <div className="users-who-liked-loading">
                         </div>
-                        {arrayOfImageLikes.map(element => (
+                        {newArray.map(element => (
                             <div className="users-who-liked-loading">
                                 <div className="modal-profile-pic" style={
-                                    { backgroundImage: `url(${element.profileImgUrl})` }
+                                    { backgroundImage: `url(${element.user.profileImgUrl})` }
                                 }></div>
-                                <div className="modal-profile-username">{element.username}</div>
+                                <div className="modal-profile-username">{element.user.username}</div>
                             </div>
                         ))}
                     </div>
