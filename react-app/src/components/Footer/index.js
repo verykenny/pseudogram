c6537b5dc61502fdb3a2a9209107ceb5b3200e09
verykenny <16845292+verykenny@users.noreply.github.com>
2021-09-18 15:40:53 -0600

    added responsive styles to footer

--- react-app/src/components/Footer/index.js
+++ react-app/src/components/Footer/index.js
@@ -6,18 +6,20 @@ const Footer = () => {
     return (
         <footer>
             <div>
-                <p className='copy'>&copy; Copyright 2021, veryKenny-Labs</p>
-            </div>
-            <div className="about-us__footer">
                 <div>
-                    <p>Curtis Bridge</p>
-                    <i className="fab fa-github-square icon"></i>
-                    <i className="fab fa-linkedin icon"></i>
+                    <p className='copy'>&copy; Copyright 2021, curtis-and-kenny</p>
                 </div>
-                <div>
-                    <p>Kenneth Donahue</p>
-                    <i className="fab fa-github-square icon"></i>
-                    <i className="fab fa-linkedin icon"></i>
+                <div className="about-us__footer">
+                    <div>
+                        <p>Curtis Bridge</p>
+                        <i className="fab fa-github-square icon"></i>
+                        <i className="fab fa-linkedin icon"></i>
+                    </div>
+                    <div>
+                        <p>Kenneth Donahue</p>
+                        <i className="fab fa-github-square icon"></i>
+                        <i className="fab fa-linkedin icon"></i>
+                    </div>
                 </div>
             </div>
         </footer>
